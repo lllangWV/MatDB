51ac19fd4e2ac3ec55dfdb2225f655ebb837a05e
lllangWV <lllangWV@gmail.com>
2024-10-02 00:03:50 -0400

    test

--- scripts/processing_git_log.py
+++ scripts/processing_git_log.py
@@ -36,4 +36,4 @@ def bash_command(command):
 
 if __name__ == "__main__":
     # Example usage:
-    print(bash_command('git tag --sort=-creatordate | head -n 1'))
+    print(bash_command('git tag'))
