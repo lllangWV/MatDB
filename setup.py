2a2219a4f10a6f94d1b18c7b23a3d5e0163e1678
lllangWV <lllangWV@gmail.com>
2024-10-02 17:17:20 -0400

    debugging

--- setup.py
+++ setup.py
@@ -1,14 +1,9 @@
 from setuptools import setup
 from setuptools_scm import ScmVersion
-import setuptools_scm
+
 def version_for_project(version: ScmVersion) -> str:
-   print(f"setup.py:version: {version.tag}")
    return str(version.tag)
    
-import pkg_resources
-print(pkg_resources.get_distribution('setuptools_scm').version)
-print(pkg_resources.get_distribution('setuptools').version)
 
 
-print("setup.py: Starting setup")
 setup(use_scm_version={'version_scheme':version_for_project})
