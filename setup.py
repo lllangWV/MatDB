28571c4843f3ba831f299ced7ddbc8d31b8a1679
lllangWV <lllangWV@gmail.com>
2024-10-02 14:37:57 -0400

    debugging version up date

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 from setuptools_scm import ScmVersion
 
 def version_for_project(version: ScmVersion) -> str:
-   print(version.tag)
+   print("setup.py:version: {version.tag}")
    return str(version.tag)
    
 
