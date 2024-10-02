ff4a4b5cf098e463190bec2cd76778f21f5159c5
lllangWV <lllangWV@gmail.com>
2024-10-01 23:50:53 -0400

    test

--- .github/scripts/processing_git_log.py
+++ .github/scripts/processing_git_log.py
@@ -1,15 +1,29 @@
 import sys
 
-def main(version, commit_log):
-    print(f"Current Version: {version}")
-    print("Commit Messages since the last version:")
-    print(commit_log)
+
+
+
+import subprocess
+
+def run_git_command(command):
+    try:
+        result = subprocess.run(['git'] + command.split(), 
+                                check=True, 
+                                capture_output=True, 
+                                text=True)
+        return result.stdout
+    except subprocess.CalledProcessError as e:
+        print(f"An error occurred: {e}")
+        return e.stderr
+
+
+
+# def main(version, commit_log):
+#     print(f"Current Version: {version}")
+#     print("Commit Messages since the last version:")
+#     print(commit_log)
 
 if __name__ == "__main__":
-    if len(sys.argv) != 3:
-        print("Usage: python script.py <version> <commit_log>")
-        sys.exit(1)
-    
-    version = sys.argv[1]
-    commit_log = sys.argv[2]
-    main(version, commit_log)
+    # Example usage:
+    print(run_git_command('status'))
+    print(run_git_command('log --oneline -n 5'))
